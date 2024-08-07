9fbe8e2f4a978486b4ae7edbaac8cf16b27c40db
Cyber MacGeddon <cybermaggedon@gmail.com>
2024-08-07 20:37:54 +0100

    v0.5.8

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 # VERSION=$(shell git describe | sed 's/^v//')
-VERSION=0.5.7
+VERSION=0.5.8
 
 all: container
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.5.7"
+version = "0.5.8"
 
 setuptools.setup(
     name="trustgraph",
