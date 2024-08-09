d94f730dfdcc2380f5965d7d2c45a2fcecbcda99
JackColquitt <daniel@kalntera.ai>
2024-08-08 20:24:33 -0700

    Preliminary OpenAI support

---
+++ trustgraph/model/text_completion/openai/llm.py
@@ -0,0 +1,114 @@
+
+"""
+Simple LLM service, performs text prompt completion using OpenAI.
+Input is prompt, output is response.
+"""
+
+from openai import OpenAI
+
+from .... schema import TextCompletionRequest, TextCompletionResponse
+from .... schema import text_completion_request_queue
+from .... schema import text_completion_response_queue
+from .... log_level import LogLevel
+from .... base import ConsumerProducer
+
+module = ".".join(__name__.split(".")[1:-1])
+
+default_input_queue = text_completion_request_queue
+default_output_queue = text_completion_response_queue
+default_subscriber = module
+default_model = 'gpt-3.5-turbo'
+
+class Processor(ConsumerProducer):
+
+    def __init__(self, **params):
+    
+        input_queue = params.get("input_queue", default_input_queue)
+        output_queue = params.get("output_queue", default_output_queue)
+        subscriber = params.get("subscriber", default_subscriber)
+        model = params.get("model", default_model)
+        api_key = params.get("api_key")
+
+        super(Processor, self).__init__(
+            **params | {
+                "input_queue": input_queue,
+                "output_queue": output_queue,
+                "subscriber": subscriber,
+                "input_schema": TextCompletionRequest,
+                "output_schema": TextCompletionResponse,
+                "model": model,
+            }
+        )
+
+        self.model = model
+
+        self.openai = OpenAI(api_key=api_key)
+
+        print("Initialised", flush=True)
+
+    def handle(self, msg):
+
+        v = msg.value()
+
+        # Sender-produced ID
+
+        id = msg.properties()["id"]
+
+        print(f"Handling prompt {id}...", flush=True)
+
+        prompt = v.prompt
+        resp = self.openai.chat.completions.create(
+            model=self.model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": prompt
+                        }
+                    ]
+                }
+            ],
+            temperature=0,
+            max_tokens=4096,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            response_format={
+                "type": "text"
+            }
+        )
+
+        print(resp, flush=True)
+
+        print("Send response...", flush=True)
+        r = TextCompletionResponse(response=resp)
+        self.send(r, properties={"id": id})
+
+        print("Done.", flush=True)
+
+    @staticmethod
+    def add_args(parser):
+
+        ConsumerProducer.add_args(
+            parser, default_input_queue, default_subscriber,
+            default_output_queue,
+        )
+
+        parser.add_argument(
+            '-m', '--model',
+            default="gpt-3.5-turbo",
+            help=f'LLM model (default: GPT-3.5-Turbo)'
+        )
+
+        parser.add_argument(
+            '-k', '--api-key',
+            help=f'OpenAI API key'
+        )
+
+def run():
+
+    Processor.start(module, __doc__)
+
+    
