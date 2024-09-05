344575959852b6390d4d4ad93ce0dfdc48ac73a0
cybermaggedon <cybermaggedon@gmail.com>
2024-09-05 20:00:38 +0100

    Fix bedrock replace (#56)
    * Fix replace ignoring first output
    
    * Bump version

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 # VERSION=$(shell git describe | sed 's/^v//')
-VERSION=0.9.1
+VERSION=0.9.2
 
 DOCKER=podman
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.9.1"
+version = "0.9.2"
 
 setuptools.setup(
     name="trustgraph",
