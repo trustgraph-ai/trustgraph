cc1a80876c95633072872b2947714ff90c65bdd4
Cyber MacGeddon <cybermaggedon@gmail.com>
2024-10-14 19:30:59 +0100

    Lock 0.12 to 0.12 packages min

--- trustgraph-vertexai/setup.py
+++ trustgraph-vertexai/setup.py
@@ -34,7 +34,7 @@ setuptools.setup(
     python_requires='>=3.8',
     download_url = "https://github.com/trustgraph-ai/trustgraph/archive/refs/tags/v" + version + ".tar.gz",
     install_requires=[
-        "trustgraph-base<0.13",
+        "trustgraph-base>=0.12,<0.13",
         "pulsar-client",
         "google-cloud-aiplatform",
         "prometheus-client",
--- trustgraph/setup.py
+++ trustgraph/setup.py
@@ -34,13 +34,13 @@ setuptools.setup(
     python_requires='>=3.8',
     download_url = "https://github.com/trustgraph-ai/trustgraph/archive/refs/tags/v" + version + ".tar.gz",
     install_requires=[
-        "trustgraph-base<0.13",
-        "trustgraph-bedrock<0.13",
-        "trustgraph-cli<0.13",
-        "trustgraph-embeddings-hf<0.13",
-        "trustgraph-flow<0.13",
-        "trustgraph-parquet<0.13",
-        "trustgraph-vertexai<0.13",
+        "trustgraph-base>=0.12,<0.13",
+        "trustgraph-bedrock>=0.12,<0.13",
+        "trustgraph-cli>=0.12,<0.13",
+        "trustgraph-embeddings-hf>=0.12,<0.13",
+        "trustgraph-flow>=0.12,<0.13",
+        "trustgraph-parquet>=0.12,<0.13",
+        "trustgraph-vertexai>=0.12,<0.13",
     ],
     scripts=[
     ]
