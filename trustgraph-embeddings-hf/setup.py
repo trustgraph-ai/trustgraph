a608f2be75a5f8b9e9bc59e0124434d86cab3b18
Cyber MacGeddon <cybermaggedon@gmail.com>
2024-11-11 22:28:21 +0000

    Fix setup deps

--- trustgraph-embeddings-hf/setup.py
+++ trustgraph-embeddings-hf/setup.py
@@ -34,8 +34,8 @@ setuptools.setup(
     python_requires='>=3.8',
     download_url = "https://github.com/trustgraph-ai/trustgraph/archive/refs/tags/v" + version + ".tar.gz",
     install_requires=[
-        "trustgraph-base>=0.14,<0.15",
-        "trustgraph-flow>=0.14,<0.15",
+        "trustgraph-base>=0.15,<0.16",
+        "trustgraph-flow>=0.15,<0.16",
         "torch",
         "urllib3",
         "transformers",
