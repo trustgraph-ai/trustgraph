b163d8a099c9facb0b9dbf3fb07266ea6794d755
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-01-04 22:07:31 +0000

    Added missing file

---
+++ trustgraph-flow/trustgraph/gateway/document_rag.py
@@ -0,0 +1,30 @@
+
+from .. schema import DocumentRagQuery, DocumentRagResponse
+from .. schema import document_rag_request_queue
+from .. schema import document_rag_response_queue
+
+from . endpoint import ServiceEndpoint
+from . requestor import ServiceRequestor
+
+class DocumentRagRequestor(ServiceRequestor):
+    def __init__(self, pulsar_host, timeout, auth):
+
+        super(DocumentRagRequestor, self).__init__(
+            pulsar_host=pulsar_host,
+            request_queue=document_rag_request_queue,
+            response_queue=document_rag_response_queue,
+            request_schema=DocumentRagQuery,
+            response_schema=DocumentRagResponse,
+            timeout=timeout,
+        )
+
+    def to_request(self, body):
+        return DocumentRagQuery(
+            query=body["query"],
+            user=body.get("user", "trustgraph"),
+            collection=body.get("collection", "default"),
+        )
+
+    def from_response(self, message):
+        return { "response": message.response }, True
+
