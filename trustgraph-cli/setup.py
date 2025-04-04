92825a60fb9dfab32425afa6ebf75f1c98920869
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-04-04 15:41:47 +0100

    Added CLI tg-set-prompt command, can be used to update prompts dynamically. Usage, see Shortcut #184.

--- trustgraph-cli/setup.py
+++ trustgraph-cli/setup.py
@@ -63,6 +63,7 @@ setuptools.setup(
         "scripts/tg-save-kg-core",
         "scripts/tg-save-doc-embeds",
         "scripts/tg-show-config",
+        "scripts/tg-set-prompt",
         "scripts/tg-show-tools",
         "scripts/tg-show-prompts",
     ]
