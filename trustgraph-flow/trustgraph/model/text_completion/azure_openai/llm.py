8ab0a9ace6e1b47144ab74bcd0e9036bb72832af
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-04-25 16:21:46 +0100

    Refactoring some LLM handlers

--- trustgraph-flow/trustgraph/model/text_completion/azure_openai/llm.py
+++ trustgraph-flow/trustgraph/model/text_completion/azure_openai/llm.py
@@ -9,18 +9,11 @@ from prometheus_client import Histogram
 from openai import AzureOpenAI, RateLimitError
 import os
 
-from .... schema import TextCompletionRequest, TextCompletionResponse, Error
-from .... schema import text_completion_request_queue
-from .... schema import text_completion_response_queue
-from .... log_level import LogLevel
-from .... base import ConsumerProducer
 from .... exceptions import TooManyRequests
+from .... base import LlmService, LlmResult
 
-module = "text-completion"
+default_ident = "text-completion"
 
-default_input_queue = text_completion_request_queue
-default_output_queue = text_completion_response_queue
-default_subscriber = module
 default_temperature = 0.0
 default_max_output = 4192
 default_api = "2024-12-01-preview"
@@ -28,13 +21,10 @@ default_endpoint = os.getenv("AZURE_ENDPOINT", None)
 default_token = os.getenv("AZURE_TOKEN", None)
 default_model = os.getenv("AZURE_MODEL", None)
 
-class Processor(ConsumerProducer):
+class Processor(LlmService):
 
     def __init__(self, **params):
 
-        input_queue = params.get("input_queue", default_input_queue)
-        output_queue = params.get("output_queue", default_output_queue)
-        subscriber = params.get("subscriber", default_subscriber)
         temperature = params.get("temperature", default_temperature)
         max_output = params.get("max_output", default_max_output)
 
@@ -51,30 +41,12 @@ class Processor(ConsumerProducer):
 
         super(Processor, self).__init__(
             **params | {
-                "input_queue": input_queue,
-                "output_queue": output_queue,
-                "subscriber": subscriber,
-                "input_schema": TextCompletionRequest,
-                "output_schema": TextCompletionResponse,
                 "temperature": temperature,
                 "max_output": max_output,
                 "model": model,
                 "api": api,
             }
         )
-
-        if not hasattr(__class__, "text_completion_metric"):
-            __class__.text_completion_metric = Histogram(
-                'text_completion_duration',
-                'Text completion duration (seconds)',
-                buckets=[
-                    0.25, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0,
-                    8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0,
-                    17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0,
-                    30.0, 35.0, 40.0, 45.0, 50.0, 60.0, 80.0, 100.0,
-                    120.0
-                ]
-            )
 
         self.temperature = temperature
         self.max_output = max_output
@@ -84,41 +56,31 @@ class Processor(ConsumerProducer):
             api_key=token,  
             api_version=api,
             azure_endpoint = endpoint,
-            )
+        )
 
-    async def handle(self, msg):
+    async def generate_content(self, system, prompt):
 
-        v = msg.value()
-
-        # Sender-produced ID
-
-        id = msg.properties()["id"]
-
-        print(f"Handling prompt {id}...", flush=True)
-
-        prompt = v.system + "\n\n" + v.prompt
-
+        prompt = system + "\n\n" + prompt
 
         try:
 
-            with __class__.text_completion_metric.time():
-                resp = self.openai.chat.completions.create(
-                    model=self.model,
-                    messages=[
-                        {
-                            "role": "user",
-                            "content": [
-                                {
-                                    "type": "text",
-                                    "text": prompt
-                                }
-                            ]
-                        }
-                    ],
-                    temperature=self.temperature,
-                    max_tokens=self.max_output,
-                    top_p=1,
-                )
+            resp = self.openai.chat.completions.create(
+                model=self.model,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": prompt
+                            }
+                        ]
+                    }
+                ],
+                temperature=self.temperature,
+                max_tokens=self.max_output,
+                top_p=1,
+            )
 
             inputtokens = resp.usage.prompt_tokens
             outputtokens = resp.usage.completion_tokens
@@ -127,15 +89,14 @@ class Processor(ConsumerProducer):
             print(f"Output Tokens: {outputtokens}", flush=True)
             print("Send response...", flush=True)
 
-            r = TextCompletionResponse(
-                response=resp.choices[0].message.content,
-                error=None,
-                in_token=inputtokens,
-                out_token=outputtokens,
-                model=self.model
+            r = LlmResult(
+                text = resp.choices[0].message.content,
+                in_token = inputtokens,
+                out_token = outputtokens,
+                model = self.model
             )
 
-            await self.send(r, properties={"id": id})
+            return r
 
         except RateLimitError:
 
@@ -147,35 +108,15 @@ class Processor(ConsumerProducer):
         except Exception as e:
 
             # Apart from rate limits, treat all exceptions as unrecoverable
-
             print(f"Exception: {e}")
-
-            print("Send error response...", flush=True)
-
-            r = TextCompletionResponse(
-                error=Error(
-                    type = "llm-error",
-                    message = str(e),
-                ),
-                response=None,
-                in_token=None,
-                out_token=None,
-                model=None,
-            )
-
-            await self.send(r, properties={"id": id})
-
-            self.consumer.acknowledge(msg)
+            raise e
 
         print("Done.", flush=True)
 
     @staticmethod
     def add_args(parser):
 
-        ConsumerProducer.add_args(
-            parser, default_input_queue, default_subscriber,
-            default_output_queue,
-        )
+        LlmService.add_args(parser)
 
         parser.add_argument(
             '-e', '--endpoint',
@@ -217,4 +158,4 @@ class Processor(ConsumerProducer):
 
 def run():
     
-    Processor.launch(module, __doc__)
+    Processor.launch(default_ident, __doc__)
