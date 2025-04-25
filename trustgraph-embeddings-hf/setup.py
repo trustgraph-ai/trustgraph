099018e103253df9adc0b0fa5e561151fc169279
cybermaggedon <cybermaggedon@gmail.com>
2025-04-25 19:45:02 +0100

    Update package versions (#352)

--- trustgraph-embeddings-hf/setup.py
+++ trustgraph-embeddings-hf/setup.py
@@ -34,8 +34,8 @@ setuptools.setup(
     python_requires='>=3.8',
     download_url = "https://github.com/trustgraph-ai/trustgraph/archive/refs/tags/v" + version + ".tar.gz",
     install_requires=[
-        "trustgraph-base>=0.22,<0.23",
-        "trustgraph-flow>=0.22,<0.23",
+        "trustgraph-base>=0.23,<0.24",
+        "trustgraph-flow>=0.23,<0.24",
         "torch",
         "urllib3",
         "transformers",
