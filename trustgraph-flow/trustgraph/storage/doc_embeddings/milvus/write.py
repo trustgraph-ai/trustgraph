555f6952fad725c6638406a1b32b0faa31171953
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-07-14 22:23:38 +0100

    Fixing storage and adding tests

--- trustgraph-flow/trustgraph/storage/doc_embeddings/milvus/write.py
+++ trustgraph-flow/trustgraph/storage/doc_embeddings/milvus/write.py
@@ -4,58 +4,41 @@ Accepts entity/vector pairs and writes them to a Milvus store.
 """
 
 from .... direct.milvus_doc_embeddings import DocVectors
+from .... base import DocumentEmbeddingsStoreService
 
-from .... schema import DocumentEmbeddings
-from .... schema import document_embeddings_store_queue
-from .... log_level import LogLevel
-from .... base import Consumer
-
-module = "de-write"
-
-default_input_queue = document_embeddings_store_queue
-default_subscriber = module
+default_ident = "de-write"
 default_store_uri = 'http://localhost:19530'
 
-class Processor(Consumer):
+class Processor(DocumentEmbeddingsStoreService):
 
     def __init__(self, **params):
 
-        input_queue = params.get("input_queue", default_input_queue)
-        subscriber = params.get("subscriber", default_subscriber)
         store_uri = params.get("store_uri", default_store_uri)
 
         super(Processor, self).__init__(
             **params | {
-                "input_queue": input_queue,
-                "subscriber": subscriber,
-                "input_schema": DocumentEmbeddings,
                 "store_uri": store_uri,
             }
         )
 
         self.vecstore = DocVectors(store_uri)
 
-    async def handle(self, msg):
+    async def store_document_embeddings(self, message):
 
-        v = msg.value()
+        for emb in message.chunks:
 
-        for emb in v.chunks:
-
+            if emb.chunk is None or emb.chunk == b"": continue
+            
             chunk = emb.chunk.decode("utf-8")
-            if chunk == "" or chunk is None: continue
+            if chunk == "": continue
 
             for vec in emb.vectors:
-
-                if chunk != "" and v.chunk is not None:
-                    for vec in v.vectors:
-                        self.vecstore.insert(vec, chunk)
+                self.vecstore.insert(vec, chunk)
 
     @staticmethod
     def add_args(parser):
 
-        Consumer.add_args(
-            parser, default_input_queue, default_subscriber,
-        )
+        DocumentEmbeddingsStoreService.add_args(parser)
 
         parser.add_argument(
             '-t', '--store-uri',
@@ -65,5 +48,5 @@ class Processor(Consumer):
 
 def run():
 
-    Processor.launch(module, __doc__)
+    Processor.launch(default_ident, __doc__)
 
