521018230ce5e53443ff1e6b301343f4202480cd
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-07-30 23:37:48 +0100

    Fix loggin startup problems

--- trustgraph-flow/trustgraph/processing/processing.py
+++ trustgraph-flow/trustgraph/processing/processing.py
@@ -19,8 +19,7 @@ def fn(module_name, class_name, params, w):
 
     logger.info(f"Starting {module_name}...")
 
-    if "log_level" in params:
-        params["log_level"] = LogLevel(params["log_level"])
+    # log_level is already a string, no conversion needed
 
     while True:
 
