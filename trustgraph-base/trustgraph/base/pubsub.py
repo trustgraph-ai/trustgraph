069bae7e774fba3c4fe9562cf1725d07c1701e3d
cybermaggedon <cybermaggedon@gmail.com>
2025-07-31 00:01:08 +0100

    Fix logging startup problems (#446)

--- trustgraph-base/trustgraph/base/pubsub.py
+++ trustgraph-base/trustgraph/base/pubsub.py
@@ -21,7 +21,7 @@ class PulsarClient:
             "pulsar_api_key",
             self.default_pulsar_api_key
         )
-        log_level = params.get("log_level", LogLevel.INFO)
+        # Hard-code Pulsar logging to ERROR level to minimize noise
 
         self.pulsar_host = pulsar_host
         self.pulsar_api_key = pulsar_api_key
@@ -31,13 +31,13 @@ class PulsarClient:
             self.client = pulsar.Client(
                 pulsar_host,
                 authentication=auth,
-                logger=pulsar.ConsoleLogger(log_level.to_pulsar())
+                logger=pulsar.ConsoleLogger(_pulsar.LoggerLevel.Error)
             )
         else:
             self.client = pulsar.Client(
                 pulsar_host,
                 listener_name=pulsar_listener,
-                logger=pulsar.ConsoleLogger(log_level.to_pulsar())
+                logger=pulsar.ConsoleLogger(_pulsar.LoggerLevel.Error)
             )
 
         self.pulsar_listener = pulsar_listener
