cba6d653d192ae91f4ad2f524ca4521afa5686ef
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-08-21 12:58:51 +0100

    Defensive

--- trustgraph-flow/trustgraph/agent/react/agent_manager.py
+++ trustgraph-flow/trustgraph/agent/react/agent_manager.py
@@ -106,10 +106,10 @@ class AgentManager:
                 action = line[7:].strip()
 
                 # Get rid of quotation prefix/suffix if present
-                while action[0] == '"':
+                while action and action[0] == '"':
                     action = action[1:]
 
-                while action[-1] == '"':
+                while action and action[-1] == '"':
                     action = action[:-1]
             
             # Parse Args
