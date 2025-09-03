5301960e8b9a6e422d1fe94786189627d6684a14
Cyber MacGeddon <cybermaggedon@gmail.com>
2025-09-03 22:12:52 +0100

    Gateway support for objects-query

---
+++ trustgraph-flow/trustgraph/gateway/dispatch/objects_query.py
@@ -0,0 +1,30 @@
+from ... schema import ObjectsQueryRequest, ObjectsQueryResponse
+from ... messaging import TranslatorRegistry
+
+from . requestor import ServiceRequestor
+
+class ObjectsQueryRequestor(ServiceRequestor):
+    def __init__(
+            self, pulsar_client, request_queue, response_queue, timeout,
+            consumer, subscriber,
+    ):
+
+        super(ObjectsQueryRequestor, self).__init__(
+            pulsar_client=pulsar_client,
+            request_queue=request_queue,
+            response_queue=response_queue,
+            request_schema=ObjectsQueryRequest,
+            response_schema=ObjectsQueryResponse,
+            subscription = subscriber,
+            consumer_name = consumer,
+            timeout=timeout,
+        )
+
+        self.request_translator = TranslatorRegistry.get_request_translator("objects-query")
+        self.response_translator = TranslatorRegistry.get_response_translator("objects-query")
+
+    def to_request(self, body):
+        return self.request_translator.to_pulsar(body)
+
+    def from_response(self, message):
+        return self.response_translator.from_response_with_completion(message)
