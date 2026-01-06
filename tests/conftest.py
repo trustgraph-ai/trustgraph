4b796ff57f6a6fd9c18d790a54cc6e53f8780ca5
Cyber MacGeddon <cybermaggedon@gmail.com>
2026-01-06 22:04:41 +0000

    Comment out debug, left in for use if needed

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,20 +5,17 @@ This conftest.py applies to all test directories.
 """
 
 import pytest
-import asyncio
-import tracemalloc
-import warnings
+# import asyncio
+# import tracemalloc
+# import warnings
 from unittest.mock import MagicMock
 
-# Enable tracemalloc immediately at import time
-tracemalloc.start()
-
-# Enable asyncio debug mode
-asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
-
-# Make warnings verbose
-warnings.simplefilter("always", ResourceWarning)
-warnings.simplefilter("always", RuntimeWarning)
+# Uncomment the lines below to enable asyncio debug mode and tracemalloc
+# for tracing unawaited coroutines and their creation points
+# tracemalloc.start()
+# asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
+# warnings.simplefilter("always", ResourceWarning)
+# warnings.simplefilter("always", RuntimeWarning)
 
 
 @pytest.fixture(scope="session", autouse=True)
