7ca41fa89314705839ef77026ef3c0620ca493ea
Jacob Molz <jmolz12@gmail.com>
2026-05-26 09:47:22 -0400

    fix: avoid swallowing cache directory errors

--- trustgraph-cli/trustgraph/cli/load_sample_documents.py
+++ trustgraph-cli/trustgraph/cli/load_sample_documents.py
@@ -4,9 +4,10 @@ Loads a PDF document into the library
 
 import argparse
 import os
-import uuid
 import datetime
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.response import HTTPResponse
 
 from trustgraph.api import Api
 from trustgraph.api.types import hash, Uri, Literal, Triple
@@ -15,9 +16,6 @@ default_url = os.getenv("TRUSTGRAPH_URL", 'http://localhost:8088/')
 default_token = os.getenv("TRUSTGRAPH_TOKEN", None)
 default_workspace = os.getenv("TRUSTGRAPH_WORKSPACE", "default")
 
-
-from requests.adapters import HTTPAdapter
-from urllib3.response import HTTPResponse
 
 class FileAdapter(HTTPAdapter):
     def send(self, request, *args, **kwargs):
@@ -28,10 +26,7 @@ session = requests.session()
 
 session.mount('file://', FileAdapter())
 
-try:
-    os.mkdir("doc-cache")
-except:
-    pass
+os.makedirs("doc-cache", exist_ok=True)
 
 documents = [
 
